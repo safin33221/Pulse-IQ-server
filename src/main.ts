7a0012d70dd3765680fecc39a45e79ce8214436e
safin33221 <safin33221@gmail.com>
2026-08-30 15:26:05 +0600

    fix(main): bind server to 0.0.0.0 for external access

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
